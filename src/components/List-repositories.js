e0f15815c7976f241222fc95a89156bd61b842be
viniciospereirarodrigues@gmail.com <viniciospereirarodrigues@gmail.com>
2021-09-30 17:23:09 -0300

    Criando-lista-de-repositorios

---
+++ src/components/List-repositories.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Reposiories = () => {
+    return (
+        <>
+        <section className="wrapper-list">
+            <h1> Nome do repositorio</h1>
+            <p> descrição do repositorio</p>
+        </section>
+            
+        </>
+    )
+}
